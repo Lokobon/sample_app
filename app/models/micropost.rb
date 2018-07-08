8f868db3693e71d3f318bdeb011d718aa8b55fc1
Lokobon <konovalov_navi@yahoo.com>
2018-07-09 01:26:16 +0500

    all user microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,16 +4,12 @@ class Micropost < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: 160 }
-  validate :picture_size
+  #validate  :picture_size
 
 
   private
 
-def picture_size
-  if picture_size > 5.megabytes
-    errors.add(:picture, "Должно быть менее 5MB")
-  end
-end
+
 
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ User.create!(name:  "123",
                password_confirmation: password)
 end
 
-users = User.order(:created_at).take(6)
+users = User.order(:created_at).take(8)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
